001e0dedbef409571fda6eb12f41a7c9dc4c6ffa
haosutang <haosutang@gmail.com>
2016-05-04 01:10:40 -0400

    naive, cython, nogil, sparse

--- work/Tang/MatrixDot.py
+++ work/Tang/MatrixDot.py
@@ -1,3 +1,6 @@
+import time
+import numpy as np
+
 class Matrix(object):
     
     def __init__(self, array):
@@ -46,3 +49,40 @@ class Matrix(object):
             result.append(thisrow)
             
         return result
+    
+    
+
+def timeit(method):
+
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+
+        print '%r %d nanoseconds' % \
+              (method.__name__, int((te-ts)*1e9))
+        return result
+
+    return timed
+
+if __name__ == '__main__':
+    A_numpy = np.random.uniform(-200000,200000, size = (100,200))
+    B_numpy = np.random.uniform(-200000,200000, size = (200,300))
+    A = list(A_numpy)
+    B = list(B_numpy)
+    
+    @timeit
+    def naive_multiplication(A, B):
+        A_mat = Matrix(A)
+        B_mat = Matrix(B)
+
+        return A_mat.dot_elementwise(B_mat)
+    
+    @timeit
+    def numpy_multiplication(A, B):
+        return np.array(A).dot(np.array(B))
+    
+    naive_result = naive_multiplication(A,B)
+    numpy_result = numpy_multiplication(A,B)
+    
+    print 'Approximately equal: ', np.allclose(naive_result, numpy_result)
